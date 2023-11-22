d08151f4aa2a7a3dced8ea1eeccc4a98fe00c22d
yaseen <yaseen00khan0007@gmail.com>
2023-11-22 15:01:31 +0530

    minimal changes

---
+++ client/src/Pages/Aboutus.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Aboutus() {
+  return (
+    <>
+        <div className='grid  grid-cols-2 '>
+            <div className='flex flex-col'>
+                <h1></h1>
+                <p></p>
+                <button></button>
+            </div>
+            <div></div>
+        </div>
+    </>
+  )
+}
+
+export default Aboutus
